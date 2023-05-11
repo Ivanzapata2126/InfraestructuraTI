6812fcdd39ca172fb80780752ca1b78c3ee9a3cc
Ivan Zapata <ivanzapata2126@gmail.com>
2023-05-11 11:37:27 -0500

    d

--- main.tf
+++ main.tf
@@ -74,6 +74,43 @@ resource "aws_security_group" "example" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+resource "aws_ecs_cluster" "utbapp" {
+  name = "utbapp"
+}
+
+resource "aws_ecs_task_definition" "utbapp" {
+  family                   = "utbapp"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([{
+    name      = "utbapp"
+    image     = "ivanzapata2126/utbapp:${var.imagebuild}"
+    essential = true
+    portMappings = [{
+      containerPort = 80
+      hostPort      = 80
+      protocol      = "tcp"
+    }]
+  }])
+}
+
+resource "aws_ecs_service" "utbapp" {
+  name            = "utbapp"
+  cluster         = aws_ecs_cluster.utbapp.id
+  task_definition = aws_ecs_task_definition.utbapp.arn
+  desired_count   = 1
+
+  network_configuration {
+    subnets         = [aws_subnet.example.id]
+    security_groups = [aws_security_group.example.id]
+    assign_public_ip = true
+  }
+
+  launch_type     = "FARGATE"
+}
 
 resource "aws_lb_target_group" "example" {
   name_prefix = "ele"
