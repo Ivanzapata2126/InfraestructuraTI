d51b4be45c742d35a38369802aa53db8b36490b9
Ivan Zapata <ivanzapata2126@gmail.com>
2023-05-11 15:52:07 -0500

    2

--- main.tf
+++ main.tf
@@ -132,7 +132,7 @@ resource "aws_security_group" "service_security_group" {
     to_port   = 0
     protocol  = "-1"
     # Only allowing traffic in from the load balancer security group
-    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+    security_groups = [aws_security_group.example.id]
   }
 
   egress {
