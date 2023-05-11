70a7ff22ee73f32f1e0e8b3ebb34a6309f23f29d
Ivan Zapata <ivanzapata2126@gmail.com>
2023-05-11 16:15:51 -0500

    dd

--- main.tf
+++ main.tf
@@ -6,15 +6,15 @@ resource "aws_default_vpc" "default_vpc" {
 }
 
 resource "aws_default_subnet" "default_subnet_a" {
-  availability_zone = "eu-west-2a"
+  availability_zone = "us-west-2a"
 }
 
 resource "aws_default_subnet" "default_subnet_b" {
-  availability_zone = "eu-west-2b"
+  availability_zone = "us-west-2b"
 }
 
 resource "aws_default_subnet" "default_subnet_c" {
-  availability_zone = "eu-west-2c"
+  availability_zone = "us-west-2c"
 }
 
 resource "aws_security_group" "example" {
