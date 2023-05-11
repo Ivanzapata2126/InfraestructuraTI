ecc88e6b9e24960fd0175061648c8886a4e2e1c5
Ivan Zapata <ivanzapata2126@gmail.com>
2023-05-11 16:08:39 -0500

    dd

--- main.tf
+++ main.tf
@@ -2,15 +2,21 @@ provider "aws" {
   region = "us-west-2"
 }
 
-data "aws_caller_identity" "current" {}
-
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "example-vpc"
-  }
+resource "aws_default_vpc" "default_vpc" {
 }
+
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "eu-west-2a"
+}
+
+resource "aws_default_subnet" "default_subnet_b" {
+  availability_zone = "eu-west-2b"
+}
+
+resource "aws_default_subnet" "default_subnet_c" {
+  availability_zone = "eu-west-2c"
+}
+
 
 resource "aws_internet_gateway" "example" {
   vpc_id = aws_vpc.example.id
@@ -20,48 +26,8 @@ resource "aws_internet_gateway" "example" {
   }
 }
 
-resource "aws_route_table" "example" {
-  vpc_id = aws_vpc.example.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.example.id
-  }
 
-  tags = {
-    Name = "example-rt"
-  }
-}
-
-resource "aws_subnet" "example" {
-  vpc_id     = aws_vpc.example.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
-
-  tags = {
-    Name = "example-subnet"
-  }
-}
-
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.example.id
-  route_table_id = aws_route_table.example.id
-}
-
-resource "aws_subnet" "example2" {
-  vpc_id     = aws_vpc.example.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "us-west-2b"
-
-  tags = {
-    Name = "example-subnet-2"
-  }
-}
-
-resource "aws_route_table_association" "b" {
-  subnet_id      = aws_subnet.example2.id
-  route_table_id = aws_route_table.example.id
-}
 
 resource "aws_security_group" "example" {
   ingress {
@@ -105,26 +71,6 @@ resource "aws_ecs_task_definition" "utbapp" {
   }])
 }
 
-resource "aws_ecs_service" "utbapp" {
-  name            = "utbapp"
-  cluster         = aws_ecs_cluster.utbapp.id
-  task_definition = aws_ecs_task_definition.utbapp.arn
-  desired_count   = 3
-
-  network_configuration {
-    subnets         = [aws_subnet.example.id]
-    security_groups  = [aws_security_group.service_security_group.id]
-    assign_public_ip = true
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.example.arn
-    container_name   = aws_ecs_task_definition.utbapp.family
-    container_port   = 3000
-  }
-
-  launch_type     = "FARGATE"
-}
 
 resource "aws_security_group" "service_security_group" {
   ingress {
@@ -148,7 +94,7 @@ resource "aws_lb_target_group" "example" {
   port        = 80
   protocol    = "HTTP"
   target_type = "ip"
-  vpc_id      = aws_vpc.example.id
+  vpc_id      = aws_default_vpc.default_vpc.id
 }
 
 resource "aws_lb_listener" "listener" {
@@ -164,18 +110,33 @@ resource "aws_lb_listener" "listener" {
 resource "aws_lb" "example" {
   name               = "example-lb"
   load_balancer_type = "application"
+  subnets            = [aws_default_subnet.default_subnet_a.id,aws_default_subnet.default_subnet_b.id,aws_default_subnet.default_subnet_c.id]
 
-  subnet_mapping {
-    subnet_id = aws_subnet.example2.id
-  }
-
-  subnet_mapping {
-    subnet_id = aws_subnet.example.id
-  }
 
   security_groups = [
     aws_security_group.example.id,
   ]
+}
+
+resource "aws_ecs_service" "utbapp" {
+  name            = "utbapp"
+  cluster         = aws_ecs_cluster.utbapp.id
+  task_definition = aws_ecs_task_definition.utbapp.arn
+  desired_count   = 3
+
+  network_configuration {
+    subnets         = [aws_default_subnet.default_subnet_a.id,aws_default_subnet.default_subnet_b.id,aws_default_subnet.default_subnet_c.id]
+    security_groups  = [aws_security_group.service_security_group.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.example.arn
+    container_name   = aws_ecs_task_definition.utbapp.family
+    container_port   = 3000
+  }
+
+  launch_type     = "FARGATE"
 }
 
 output "app_url" {
