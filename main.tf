7c766bfbbb330e177dd7492bb43e156d35a6d2d2
Ivan Zapata <ivanzapata2126@gmail.com>
2023-05-10 19:41:11 -0500

    d

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_security_group" "example" {
 }
 
 resource "aws_lb_target_group" "example" {
-  name_prefix = "example"
+  name_prefix = "expl"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.example.id
