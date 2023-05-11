c6726b991768aa86425862dbcf6d703fca3558d1
Ivan Zapata <ivanzapata2126@gmail.com>
2023-05-10 21:11:08 -0500

    fff

--- main.tf
+++ main.tf
@@ -12,6 +12,27 @@ resource "aws_vpc" "example" {
   }
 }
 
+resource "aws_internet_gateway" "example" {
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    Name = "example-igw"
+  }
+}
+
+resource "aws_route_table" "example" {
+  vpc_id = aws_vpc.example.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example.id
+  }
+
+  tags = {
+    Name = "example-rt"
+  }
+}
+
 resource "aws_subnet" "example" {
   vpc_id     = aws_vpc.example.id
   cidr_block = "10.0.1.0/24"
@@ -20,6 +41,7 @@ resource "aws_subnet" "example" {
   tags = {
     Name = "example-subnet"
   }
+  route_table_id = aws_route_table.example.id
 }
 
 resource "aws_subnet" "example2" {
@@ -30,6 +52,8 @@ resource "aws_subnet" "example2" {
   tags = {
     Name = "example-subnet-2"
   }
+  route_table_id = aws_route_table.example.id
+
 }
 
 resource "aws_security_group" "example" {
