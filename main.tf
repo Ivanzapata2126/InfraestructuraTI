873f2e1c7321684357bac426f2329246b3287f26
Ivan Zapata <ivanzapata2126@gmail.com>
2023-05-11 21:02:36 -0500

    fdf

--- main.tf
+++ main.tf
@@ -47,8 +47,6 @@ resource "aws_ecs_task_definition" "utbapp" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
-
 
   container_definitions = jsonencode([{
     name      = "utbapp"
@@ -59,31 +57,6 @@ resource "aws_ecs_task_definition" "utbapp" {
       hostPort      = 3000
     }]
   }])
-}
-
-data "aws_iam_role" "ecsTaskExecutionRole" {
-  name = "ecsTaskExecutionRole"
-}
-
-resource "aws_iam_role" "ecsTaskExecutionRole" {
-  name = "ecsTaskExecutionRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  role       = data.aws_iam_role.ecsTaskExecutionRole.arn
 }
 
 
