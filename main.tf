e72885578911f1cf12fdc6c5cb38e524b57f2911
Ivan Zapata <ivanzapata2126@gmail.com>
2023-05-11 20:52:22 -0500

    df

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ resource "aws_ecs_task_definition" "utbapp" {
 
 resource "aws_iam_role" "ecsTaskExecutionRole" {
   name               = "ecsTaskExecutionRole"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  assume_role_policy = data.aws_iam_role.ecsTaskExecutionRole.json
 }
 
 data "aws_iam_role" "ecsTaskExecutionRole" {
