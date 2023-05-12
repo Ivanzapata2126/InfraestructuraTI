145b9b6e7f0414656c2263a70080c16815639e4f
Ivan Zapata <ivanzapata2126@gmail.com>
2023-05-11 21:14:59 -0500

    ggg

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ resource "aws_ecs_task_definition" "utbapp" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRol.arn
 
 
   container_definitions = jsonencode([{
