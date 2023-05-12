882df51663365f0ea0ceeda735537026621c34de
Ivan Zapata <ivanzapata2126@gmail.com>
2023-05-11 20:45:54 -0500

    dsada

--- main.tf
+++ main.tf
@@ -61,6 +61,11 @@ resource "aws_ecs_task_definition" "utbapp" {
   }])
 }
 
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
 data "aws_iam_role" "ecsTaskExecutionRole" {
   name = "ecsTaskExecutionRole"
 }
