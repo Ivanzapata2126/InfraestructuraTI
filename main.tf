7c08781e8d1962ac12125cfbf14c60288ca23a34
Ivan Zapata <ivanzapata2126@gmail.com>
2023-05-11 21:11:35 -0500

    dfsd

--- main.tf
+++ main.tf
@@ -40,6 +40,26 @@ variable "imagebuild" {
   type = string
   description = "the latest image build version"
 }
+resource "aws_iam_role" "ecsTaskExecutionRol" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = aws_iam_role.ecsTaskExecutionRol.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
 
 resource "aws_ecs_task_definition" "utbapp" {
   family                   = "utbapp"
@@ -47,6 +67,8 @@ resource "aws_ecs_task_definition" "utbapp" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
+
 
   container_definitions = jsonencode([{
     name      = "utbapp"
