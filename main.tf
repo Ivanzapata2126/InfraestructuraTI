e97a5497875a8ffea82ae46e521f2f1f83a581d6
anderson morillo <54423269+andersonmorillo@users.noreply.github.com>
2023-05-13 15:20:34 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,56 +1,78 @@
 terraform {
-  backend "s3" {
-    bucket = "terraformgrupo02state"
-    key    = "terraform.tfstate"
-    region = "us-west-2"
+  required_providers {
+    docker = {
+      source    = "kreuzwerker/docker"
+      version   = "~> 2.20.0"
+    }
+    aws = {
+      source    = "hashicorp/aws"
+      version   = "~> 4.16"
+    }
   }
 }
 
 provider "aws" {
-  region = "us-west-2"
+ 
 }
 
-resource "aws_default_vpc" "default_vpc" {
-}
-
-resource "aws_default_subnet" "default_subnet_a" {
-  availability_zone = "us-west-2a"
-}
-
-resource "aws_default_subnet" "default_subnet_b" {
-  availability_zone = "us-west-2b"
-}
-
-resource "aws_default_subnet" "default_subnet_c" {
-  availability_zone = "us-west-2c"
-}
-
-resource "aws_security_group" "example" {
-  ingress {
-    from_port   = 80 # Allowing traffic in from port 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
-  }
-
-  egress {
-    from_port   = 0 # Allowing any incoming port
-    to_port     = 0 # Allowing any outgoing port
-    protocol    = "-1" # Allowing any outgoing protocol 
-    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
-  }
-}
-resource "aws_ecs_cluster" "utbapp" {
-  name = "utbapp"
-}
 
 variable "imagebuild" {
   type = string
   description = "the latest image build version"
 }
-resource "aws_iam_role" "ecsTaskExecutionRol" {
-  name_prefix               = "ecs"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+
+
+# Providing a reference to our default VPC
+resource "aws_default_vpc" "default_vpc" {
+}
+
+# Providing a reference to our default subnets
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_default_subnet" "default_subnet_b" {
+  availability_zone = "us-east-1b"
+}
+
+resource "aws_ecr_repository" "my_ecr_repo" {
+  name = "actividad1corte3${var.imagebuild}"
+}
+
+resource "aws_ecs_cluster" "my_cluster" {
+  name = "my-cluster${var.imagebuild}" # Naming the cluster
+}
+
+resource "aws_ecs_task_definition" "my_first_task" {
+  family                   = "ACTVIDAD1${var.imagebuild}" # Naming our first task
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "ACTVIDAD1${var.imagebuild}",
+      "image": "ivanzapata2126/utbapp:${var.imagebuild}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 80,
+          "hostPort": 80,
+          "protocol": "tcp"
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
+  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
+  memory                   = 512         # Specifying the memory our container requires
+  cpu                      = 256        # Specifying the CPU our container requires
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole${var.imagebuild}"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
 }
 
 data "aws_iam_policy_document" "assume_role_policy" {
@@ -65,28 +87,74 @@ data "aws_iam_policy_document" "assume_role_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
-  role       = aws_iam_role.ecsTaskExecutionRol.name
+  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "aws_ecs_task_definition" "utbapp" {
-  family                   = "utbapp"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecsTaskExecutionRol.arn
+resource "aws_alb" "application_load_balancer" {
+  name               = "load-balancer-dev${var.imagebuild}" # Naming our load balancer
+  load_balancer_type = "application"
+  subnets = [ # Referencing the default subnets
+    "${aws_default_subnet.default_subnet_a.id}",
+    "${aws_default_subnet.default_subnet_b.id}"
+  ]
+  # Referencing the security group
+  security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
+}
 
+# Creating a security group for the load balancer:
+resource "aws_security_group" "load_balancer_security_group" {
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
+  }
 
-  container_definitions = jsonencode([{
-    name      = "nuevo"
-    image     = "393450248593.dkr.ecr.us-west-2.amazonaws.com/grupo02:111dc73e31e2ee4929ab9e38cfe809d50163dfc3"
-    essential = true
-    portMappings = [{
-      containerPort = 3000
-      hostPort      = 3000
-    }]
-  }])
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "target-group${var.imagebuild}"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = "${aws_default_vpc.default_vpc.id}" # Referencing the default VPC
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" # Referencing our load balancer
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our tagrte group
+  }
+}
+
+resource "aws_ecs_service" "my_first_service" {
+  name     =  "actividad1corte3${var.imagebuild}"                           # Naming our first service
+  cluster         = "${aws_ecs_cluster.my_cluster.id}"             # Referencing our created Cluster
+  task_definition = "${aws_ecs_task_definition.my_first_task.arn}" # Referencing the task our service will spin up
+  launch_type     = "FARGATE"
+  desired_count   = 3 # Setting the number of containers to 3
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
+    container_name   = "${aws_ecs_task_definition.my_first_task.family}"
+    container_port   = 80 # Specifying the container port
+  }
+
+  network_configuration {
+    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}"]
+    assign_public_ip = true                                                # Providing our containers with public IPs
+    security_groups  = ["${aws_security_group.service_security_group.id}"] # Setting the security group
+  }
 }
 
 
@@ -96,62 +164,23 @@ resource "aws_security_group" "service_security_group" {
     to_port   = 0
     protocol  = "-1"
     # Only allowing traffic in from the load balancer security group
-    security_groups = [aws_security_group.example.id]
+    security_groups = ["${aws_security_group.load_balancer_security_group.id}"]
   }
 
   egress {
-    from_port   = 0 # Allowing any incoming port
-    to_port     = 0 # Allowing any outgoing port
-    protocol    = "-1" # Allowing any outgoing protocol 
-    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_lb_target_group" "example" {
-  name_prefix = "ele"
-  port        = 80
-  protocol    = "HTTP"
-  target_type = "ip"
-  vpc_id      = aws_default_vpc.default_vpc.id
+#Show the load balancer url (app url)
+output "website_url" {
+  value = aws_alb.application_load_balancer.dns_name
 }
 
-resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_lb.example.arn
-  port              = "80"
-  protocol          = "HTTP"
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.example.arn
-  }
-}
-
-resource "aws_lb" "example" {
-  name               = "example-lb"
-  load_balancer_type = "application"
-  subnets            = [aws_default_subnet.default_subnet_a.id,aws_default_subnet.default_subnet_b.id,aws_default_subnet.default_subnet_c.id]
-
-
-  security_groups = [
-    aws_security_group.example.id,
-  ]
-}
-
-resource "aws_ecs_service" "utbapp" {
-  name            = "utbapp"
-  cluster         = aws_ecs_cluster.utbapp.id
-  task_definition = aws_ecs_task_definition.utbapp.arn
-  desired_count   = 3
-
-  network_configuration {
-    subnets         = [aws_default_subnet.default_subnet_a.id,aws_default_subnet.default_subnet_b.id,aws_default_subnet.default_subnet_c.id]
-    security_groups  = [aws_security_group.service_security_group.id]
-    assign_public_ip = true
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.example.arn
-    container_name   = aws_ecs_task_definition.utbapp.family
-    container_port   = 3000
-  }
-  launch_type     = "FARGATE"
+#Show the ecs cluster name
+output "cluster_name" {
+  value = aws_ecs_cluster.my_cluster.name
 }
